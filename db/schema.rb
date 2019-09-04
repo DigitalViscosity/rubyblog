f9a3e48c65a8ab0c70764f04d6445190fec644f8
Torei Miller <sylphidewings@gmail.com>
2019-09-03 23:59:18 -0400

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_09_04_034105) do
+
+  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+  end
 
 end
